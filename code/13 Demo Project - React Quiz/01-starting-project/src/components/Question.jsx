f27bf36d461bdac7673f2bd3a2c69045f487f341
Chris Herbst <cherbst@mdsol.com>
2024-09-10 11:35:27 -0400

    factor out some state.

--- code/13 Demo Project - React Quiz/01-starting-project/src/components/Question.jsx
+++ code/13 Demo Project - React Quiz/01-starting-project/src/components/Question.jsx
@@ -1,16 +1,48 @@
 import QuestionTimer from './QuestionTimer.jsx';
 import QUESTIONS from '../questions.js';
 import Answers from './Answers.jsx';
+import { useState } from 'react';
 
-export default function Question({ questionText, answers, onSelectAnswer, selectedAnswer, answerState, onSkipAnswer }) {
+export default function Question({ index, onSelectAnswer, onSkipAnswer }) {
+  const [answer, setAnswer] = useState({
+    selectedAnswer: '',
+    isCorrect: null
+  });
+
+  let answerState = '';
+
+  if (answer.selectedAnswer && answer.isCorrect !== null) {
+    answerState = answer.isCorrect ? 'correct' : 'wrong';
+  } else if (answer.selectedAnswer) {
+    answerState = 'answered';
+  }
+
+  function handleSelectAnswer(answer) {
+    setAnswer({
+      selectedAnswer: answer,
+      isCorrect: null
+    })
+
+    setTimeout(() => {
+      setAnswer({
+        selectedAnswer: answer,
+        isCorrect: QUESTIONS[index].answers[0] === answer
+      })
+
+      setTimeout(() => {
+        onSelectAnswer(answer);
+      }, 2000)
+    }, 1000);
+  }
+
   return <div id='question'>
     <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
-    <p>{questionText}</p>
+    <p>{QUESTIONS[index].text}</p>
     <Answers
-      answers={answers}
-      selectedAnswer={selectedAnswer}
+      answers={QUESTIONS[index].answers}
+      selectedAnswer={answer.selectedAnswer}
       answerState={answerState}
-      onSelect={onSelectAnswer}
+      onSelect={handleSelectAnswer}
     />
   </div>
 }
