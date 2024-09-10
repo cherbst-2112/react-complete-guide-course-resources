84234afea50e464bda77ff1166bf28b9cd20d4fb
Chris Herbst <cherbst@mdsol.com>
2024-09-10 10:55:19 -0400

    Fix erratic progress bar

---
+++ code/13 Demo Project - React Quiz/01-starting-project/src/components/Question.jsx
@@ -0,0 +1,16 @@
+import QuestionTimer from './QuestionTimer.jsx';
+import QUESTIONS from '../questions.js';
+import Answers from './Answers.jsx';
+
+export default function Question({ questionText, answers, onSelectAnswer, selectedAnswer, answerState, onSkipAnswer }) {
+  return <div id='question'>
+    <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
+    <p>{questionText}</p>
+    <Answers
+      answers={answers}
+      selectedAnswer={selectedAnswer}
+      answerState={answerState}
+      onSelect={onSelectAnswer}
+    />
+  </div>
+}
--- code/13 Demo Project - React Quiz/01-starting-project/src/components/Quiz.jsx
+++ code/13 Demo Project - React Quiz/01-starting-project/src/components/Quiz.jsx
@@ -2,7 +2,7 @@ import { useCallback, useState } from 'react';
 
 import QUESTIONS from '../questions.js';
 import quizCompleteImage from '../assets/quiz-complete.png';
-import QuestionTimer from './QuestionTimer.jsx';
+import Question from './Question.jsx';
 
 export default function Quiz() {
   const [answerState, setAnswerState] = useState('');
@@ -43,36 +43,17 @@ export default function Quiz() {
     </div>
   }
 
-  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-  shuffledAnswers.sort(() => Math.random() - 0.5);
-
   return (
     <div id='quiz'>
-      <div id='question'>
-        <QuestionTimer key={activeQuestionIndex} timeout={10000} onTimeout={handleSkipAnswer} />
-        <p>{QUESTIONS[activeQuestionIndex].text}</p>
-        <ul id='answers'>
-          {shuffledAnswers.map((answer) => {
-            const isSelected = userAnswers[userAnswers.length - 1] === answer;
-
-            let cssClasses = '';
-
-            if (answerState === 'answered' && isSelected) {
-              cssClasses = 'selected';
-            }
-
-            if ((answerState === 'correct' || answerState === 'wrong') && isSelected) {
-              cssClasses = answerState;
-            }
-
-            return <li key={answer} className='answer'>
-              <button onClick={() => handleSelectAnswer(answer)} className={cssClasses}>
-                {answer}
-              </button>
-            </li>
-          })}
-        </ul>
-      </div>
+      <Question
+        key={activeQuestionIndex}
+        questionText={QUESTIONS[activeQuestionIndex].text}
+        answers={QUESTIONS[activeQuestionIndex].answers}
+        onSelectAnswer={handleSelectAnswer}
+        selectedAnswer={userAnswers[userAnswers.length - 1]}
+        answerState={answerState}
+        onSkipAnswer={handleSkipAnswer}
+      />
     </div>
   );
 }
