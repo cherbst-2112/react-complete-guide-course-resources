f64706986f5b3b50dd383607cb154e7253d9c310
Chris Herbst <cherbst@mdsol.com>
2024-12-30 10:57:08 -0500

    Add Error.jsx.

--- code/18 Practice Project - Food Order/01-starting-project/src/components/Meals.jsx
+++ code/18 Practice Project - Food Order/01-starting-project/src/components/Meals.jsx
@@ -1,11 +1,18 @@
 import MealItem from './MealItem.jsx';
 import useHttp from '../hooks/useHttp.js';
+import Error from './Error.jsx';
 
 export default function Meals() {
   const { data: loadedMeals, isLoading, error } = useHttp({ url: 'http://localhost:3000/meals', initialData: [] });
 
   if (isLoading) {
-    return <p>Fetching...</p>
+    return <p className='center'>Fetching...</p>
+  }
+
+  console.log(error);
+
+  if (error) {
+    return <Error title='failed' message={error} />
   }
 
   return <ul id='meals'>
