62647c020e6741d841bfdf0bb9340ef1cc26d6f4
Chris Herbst <cherbst@mdsol.com>
2025-01-08 11:07:33 -0500

    Beginning cart reducer and product list.

--- code/20 Advanced Redux/01-starting-project/src/components/Shop/Products.js
+++ code/20 Advanced Redux/01-starting-project/src/components/Shop/Products.js
@@ -1,16 +1,23 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  { id: 'p1', price: 6, title: 'Test Item', description: 'a test item' },
+  { id: 'p2', price: 5, title: 'Test Item 2', description: 'a test item 2' },
+]
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map(product => (
+          <ProductItem
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
