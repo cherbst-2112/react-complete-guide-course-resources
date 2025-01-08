76f0f66de772238c51cc7269285b113c7effe432
Chris Herbst <cherbst@mdsol.com>
2025-01-08 10:39:57 -0500

    Add redux and toggle cart.

--- code/20 Advanced Redux/01-starting-project/src/components/Cart/CartButton.js
+++ code/20 Advanced Redux/01-starting-project/src/components/Cart/CartButton.js
@@ -1,8 +1,17 @@
 import classes from './CartButton.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { uiSliceActions } from '../../store/ui-slice';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const cartIsVisible = useSelector(state => state.ui.cartIsVisible);
+
+  const toggleCart = () => {
+    dispatch(uiSliceActions.toggle());
+   }
+
   return (
-    <button className={classes.button}>
+    <button onClick={toggleCart} className={classes.button}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
