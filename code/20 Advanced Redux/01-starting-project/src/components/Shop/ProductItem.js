194f866dea294d0c35714897a066174d37f3b087
Chris Herbst <cherbst@mdsol.com>
2025-01-13 10:38:38 -0500

    cart is working

--- code/20 Advanced Redux/01-starting-project/src/components/Shop/ProductItem.js
+++ code/20 Advanced Redux/01-starting-project/src/components/Shop/ProductItem.js
@@ -1,8 +1,20 @@
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { cartActions } from '../../store/cart-slice';
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+  const { id, title, price, description } = props;
+
+  const dispatch = useDispatch();
+
+  const addtoCartHandler = () => {
+    dispatch(cartActions.addItemToCart({
+      id,
+      title,
+      price
+    }));
+  };
 
   return (
     <li className={classes.item}>
@@ -13,7 +25,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={addtoCartHandler}>Add to Cart</button>
         </div>
       </Card>
     </li>
--- code/20 Advanced Redux/01-starting-project/src/components/Shop/Products.js
+++ code/20 Advanced Redux/01-starting-project/src/components/Shop/Products.js
@@ -13,6 +13,8 @@ const Products = (props) => {
       <ul>
         {DUMMY_PRODUCTS.map(product => (
           <ProductItem
+            key={product.id}
+            id={product.id}
             title={product.title}
             price={product.price}
             description={product.description}
