2662e437d9b4ce3c087d9ed9d2771227cb79c91c
Chris Herbst <cherbst@mdsol.com>
2025-02-19 10:21:51 -0500

    Action creators working.

--- code/20 Advanced Redux/01-starting-project/src/App.js
+++ code/20 Advanced Redux/01-starting-project/src/App.js
@@ -5,8 +5,7 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
-
-import { uiActions } from './store/ui-slice';
+import { sendCartData } from './store/cart-slice';
 
 let isInitial = true;
 
@@ -17,46 +16,12 @@ function App() {
   const notification = useSelector(state => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'sending',
-          message: 'sending cart data'
-        })
-      );
-
-      const response = await fetch('https://verbose-disco-default-rtdb.firebaseio.com/cart.json', {
-        method: 'PUT', body: JSON.stringify(cart)
-      });
-
-      if (!response.ok) {
-        throw new Error('sending cart');
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'success!',
-          message: 'sent cart data'
-        })
-      );
-    }
-
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch(error => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'error!',
-          message: 'sending cart data failed'
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
--- code/20 Advanced Redux/01-starting-project/src/store/cart-slice.js
+++ code/20 Advanced Redux/01-starting-project/src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { uiActions } from './ui-slice';
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -38,6 +39,48 @@ const cartSlice = createSlice({
     }
   }
 });
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: 'pending',
+        title: 'sending...',
+        message: 'sending cart data'
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch('https://verbose-disco-default-rtdb.firebaseio.com/cart.json', {
+        method: 'PUT', body: JSON.stringify(cart)
+      });
+
+      if (!response.ok) {
+        throw new Error('sending cart');
+      }
+    }
+
+    try {
+      await sendRequest();
+
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'sent',
+          message: 'sent cart data'
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'error!',
+          message: 'sending cart data failed'
+        })
+      );
+    }
+  }
+}
 
 export const cartActions = cartSlice.actions;
 
