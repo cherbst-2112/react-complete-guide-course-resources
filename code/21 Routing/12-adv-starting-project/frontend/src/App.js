478a7f4bddfbeb8001364597479677de3fc54bae
Chris Herbst <cherbst@mdsol.com>
2025-05-29 11:04:58 -0400

    navlinks working

--- code/21 Routing/12-adv-starting-project/frontend/src/App.js
+++ code/21 Routing/12-adv-starting-project/frontend/src/App.js
@@ -15,6 +15,7 @@
 // 3. Add a root layout that adds the <MainNavigation> component above all page components
 // 4. Add properly working links to the MainNavigation
 // 5. Ensure that the links in MainNavigation receive an "active" class when active
+// TODO
 // 6. Output a list of dummy events to the EventsPage
 //    Every list item should include a link to the respective EventDetailPage
 // 7. Output the ID of the selected event on the EventDetailPage
@@ -27,13 +28,20 @@ import EventsPage from './components/EventsPage';
 import EventDetailPage from './components/EventDetailPage';
 import NewEventPage from './components/NewEventPage';
 import EditEventPage from './components/EditEventPage';
+import RootLayout from './components/RootLayout';
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> },
-  { path: '/events', element: <EventsPage /> },
-  { path: '/events/new', element: <NewEventPage /> },
-  { path: '/events/:eventId', element: <EventDetailPage /> },
-  { path: '/events/:eventId/edit', element: <EditEventPage /> }
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: 'events', element: <EventsPage /> },
+      { path: 'events/new', element: <NewEventPage /> },
+      { path: 'events/:eventId', element: <EventDetailPage /> },
+      { path: 'events/:eventId/edit', element: <EditEventPage /> }
+    ]
+  }
 ]);
 
 function App() {
