a1f9e1d8d60b0c8a93fea68731ecf34e2329ae70
Chris Herbst <cherbst@mdsol.com>
2025-06-18 12:45:11 -0400

    Add placeholder loading text.

--- code/21 Routing/12-adv-starting-project/frontend/src/components/RootLayout.jsx
+++ code/21 Routing/12-adv-starting-project/frontend/src/components/RootLayout.jsx
@@ -1,10 +1,13 @@
 import MainNavigation from './MainNavigation';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 
 const RootLayout = () => {
+  // const navigation = useNavigation();
+
   return <>
     <MainNavigation />
     <main>
+      {/*{navigation.state === 'loading' && <p>Loading</p>}*/}
       <Outlet />
     </main>
   </>;
