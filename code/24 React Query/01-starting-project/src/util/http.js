a732354608da954d1580bf553059b18d387f62e8
Chris Herbst <cherbst@mdsol.com>
2026-08-31 12:43:50 -0400

    EventDetails loads.

--- code/24 React Query/01-starting-project/src/util/http.js
+++ code/24 React Query/01-starting-project/src/util/http.js
@@ -6,7 +6,7 @@ export async function fetchEvents({ signal, searchTerm }) {
   let url = 'http://localhost:3000/events';
 
   if (searchTerm) {
-    url += `?search=${searchTerm}`;
+    url += '?search=' + searchTerm;
   }
 
   const response = await fetch(url, { signal: signal });
@@ -22,6 +22,7 @@ export async function fetchEvents({ signal, searchTerm }) {
 
   return events;
 }
+
 
 export async function createNewEvent(eventData) {
   const response = await fetch(`http://localhost:3000/events`, {
@@ -57,4 +58,35 @@ export async function fetchSelectableImages({ signal }) {
   const { images } = await response.json();
 
   return images;
+}
+
+export async function fetchEvent({ id, signal }) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, { signal });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while fetching the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+
+  return event;
+}
+
+
+export async function deleteEvent({ id }) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, {
+    method: 'DELETE',
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while deleting the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  return response.json();
 }
